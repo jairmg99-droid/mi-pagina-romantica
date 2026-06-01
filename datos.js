ceb0a94d094eb2ffbe1ae70a950ace5dee38527c
jairmg99-droid <jairmg99@gmail.com>
2026-05-31 21:11:18 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,1 +1,23 @@
-
+const misFotos = [
+  { archivo: "CITA 1.jpeg", mes: "agosto", anio: "2025", descripcion: "Nuestro primer día juntos 💕" },
+  { archivo: "CITA 2.jpeg", mes: "agosto", anio: "2025", descripcion: "Estábamos nerviosos ✈️" },
+  { archivo: "CITA 3.jpeg", mes: "agosto", anio: "2025", descripcion: "Un momento lleno de risas 😄" },
+  { archivo: "CITA 4.jpeg", mes: "septiembre", anio: "2025", descripcion: "Nuestra primera cita con las manos juntas 💞" },
+  { archivo: "flores.jpeg", mes: "septiembre", anio: "2025", descripcion: "Flores y cine 🌼" },
+  { archivo: "DECLARACION.jpeg", mes: "octubre", anio: "2025", descripcion: "Nos dimos el sí uwu" },
+  { archivo: "CITA 5.jpeg", mes: "octubre", anio: "2025", descripcion: "Nuestro primer picnic 🌸" },
+  { archivo: "CITA 7.jpeg", mes: "octubre", anio: "2025", descripcion: "Muah muah 😘" },
+  { archivo: "chaufita.png", mes: "octubre", anio: "2025", descripcion: "Rico chaufita 🤤" },
+  { archivo: "tareas.jpeg", mes: "noviembre", anio: "2025", descripcion: "Haciendo tareitas 🧑‍💻" },
+  { archivo: "CITA_STICH.jpeg", mes: "noviembre", anio: "2025", descripcion: "Regalo para esos días 😊" },
+  { archivo: "CITA_FOTO.jpeg", mes: "noviembre", anio: "2025", descripcion: "Compartiendo logros 🏆" },
+  { archivo: "con la chica mas linda.jpeg", mes: "noviembre", anio: "2025", descripcion: "Con la chica más linda ❤️" },
+  { archivo: "citapl.jpeg", mes: "diciembre", anio: "2025", descripcion: "Peli juntos uwu 🎬" },
+  { archivo: "cita_metro.jpeg", mes: "diciembre", anio: "2025", descripcion: "Verguencita 🫣🥹" },
+  { archivo: "lima.jpeg", mes: "diciembre", anio: "2025", descripcion: "Navidad en Lima 🎄" },
+  { archivo: "costillitas.jpeg", mes: "diciembre", anio: "2025", descripcion: "Comimos 🐽" },
+  { archivo: "ENERO 1.jpeg", mes: "enero", anio: "2026", descripcion: "Cine 🍿" },
+  { archivo: "ENERO 2.jpeg", mes: "enero", anio: "2026", descripcion: "Granja villa 🎢" },
+  { archivo: "ENERO 3.jpeg", mes: "enero", anio: "2026", descripcion: "Arte 🖌️🎨" }
+  // 👈 Cuando subas una foto nueva a GitHub, agrégala aquí abajo siguiendo el mismo formato
+];
